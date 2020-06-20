35e2bd54c77442396721dc20e8f77b9894456a90
Bryce Pingul <brycepingul@gmail.com>
2020-06-19 20:25:03 -0700

    final changes, updated read me

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # workdayscheduler
+Github Repo: https://github.com/Brycetp11/workdayscheduler
+
+Deployed application: https://brycetp11.github.io/workdayscheduler/index.html
+
+In this project the goal was to make an hourly planner complete with an hours tab, text area, and save button for each row. The save button saved the text to local storage, and will populate into the correct hour field when the page is reloaded as before.
+
+I created all the elements after the header dynamically using jquery.
+
+The text areas will have different background color depending on the time of day.
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// create reference for the current day using id= currentDay
 var dateToday = $("#currentDay");
 dateToday.text(moment().format('dddd MMMM Do, YYYY'));
 var currentHour;
 
-// create timeblocks for hours 9am-5pm class= time-block
 var dayContainer = $(".container");
 var timeHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-var taskArray = []
+var taskArray = [];
+
 
 for (let i = 0; i < timeHours.length; i++) {
 
@@ -15,6 +14,7 @@ for (let i = 0; i < timeHours.length; i++) {
         currentHour = d.getHours();
         console.log(currentHour);
     }
+    hours();
 
     var newRow = $("<row>");
     newRow.addClass("row");
@@ -28,39 +28,92 @@ for (let i = 0; i < timeHours.length; i++) {
         thisHour.text((timeHours[i]-12) + " PM");
     } else {
         thisHour.text(timeHours[i] + " PM")
-    }
-
+    };
     newRow.append(thisHour);
 
     var textArea = $("<textarea>");
-    textArea.addClass("description time-block col-10")
-    if (timeHours[i] == moment().format('LT')){
+    textArea.addClass("description time-block col-10");
+    textArea.addClass(timeHours[i]);
+    if (timeHours[i] == currentHour){
         textArea.addClass("present");
     } else if (timeHours[i] < currentHour){
         textArea.addClass("past");
     } else if(timeHours[i] > currentHour){
         textArea.addClass("future");
     };
-    textArea.attr('id', timeHours[i]);
+
     newRow.append(textArea);
 
     var saveButton = $("<button>");
     saveButton.addClass("saveBtn i:hover col-1");
+    saveButton.attr('id', timeHours[i])
     newRow.append(saveButton);
     saveButton.append("<img src=https://img.icons8.com/metro/26/000000/save.png/>");
 
-    saveButton.click(function(){
-        var savingNow = $(this).siblings(".description").val()
-        localStorage.setItem('text', savingNow);
-        var currentText = localStorage.getItem('text');
-        $(this).siblings(timeHours[i]).append(currentText);
-        console.log(currentText);
+    $("#9").click(function(){
+        var savingNine = $(this).siblings(".description").val()
+        localStorage.setItem('data9', savingNine);
     });
+
+    $("#10").click(function(){
+        var savingTen = $(this).siblings(".description").val()
+        localStorage.setItem('data10', savingTen);
+    });
+
+    $("#11").click(function(){
+        var savingEleven = $(this).siblings(".description").val()
+        localStorage.setItem('data11', savingEleven);
+    });
+
+    $("#12").click(function(){
+        var savingTwelve = $(this).siblings(".description").val()
+        localStorage.setItem("data12", savingTwelve);
+    });
+
+    $("#13").click(function(){
+        var savingOne = $(this).siblings(".description").val()
+        localStorage.setItem("data13", savingOne);
+    });
+
+    $("#14").click(function(){
+        var savingTwo = $(this).siblings(".description").val()
+        localStorage.setItem("data14", savingTwo);
+    });
+
+    $("#15").click(function(){
+        var savingThree = $(this).siblings(".description").val()
+        localStorage.setItem("data15", savingThree);
+    });
+
+    $("#16").click(function(){
+        var savingFour = $(this).siblings(".description").val()
+        localStorage.setItem("data16", savingFour);
+    });
+    
+    $("#17").click(function(){
+        var savingFive = $(this).siblings(".description").val()
+        localStorage.setItem("data17", savingFive);
+    });
+   
 };
- 
-
-
-
-
-    // save button at the end of the row saves the text to local storage
-
+function pageStart (){
+    var textNine = localStorage.getItem("data9");
+    var textTen = localStorage.getItem("data10");
+    var textEleven = localStorage.getItem("data11");
+    var textTwelve = localStorage.getItem("data12");
+    var textOne = localStorage.getItem("data13");
+    var textTwo = localStorage.getItem("data14");
+    var textThree = localStorage.getItem("data15");
+    var textFour = localStorage.getItem("data16");
+    var textFive = localStorage.getItem("data17");
+    $(".9").append(textNine);
+    $(".10").append(textTen);
+    $(".11").append(textEleven);
+    $(".12").append(textTwelve);
+    $(".13").append(textOne);
+    $(".14").append(textTwo);
+    $(".15").append(textThree);
+    $(".16").append(textFour);
+    $(".17").append(textFive);
+    };
+pageStart();
